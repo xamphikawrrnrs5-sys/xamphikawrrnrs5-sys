464272bebbcc3bc1be8aee292565d52aa88773f9
xamphikawrrnrs5-says.github.io <xamphikawrrnrs5@gmail.com>
2026-08-25 09:03:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOWContentLoaded',function () {
+  document.getElementById('year').innerHTML = new Data().getFullYear();
   console.log('Portfolio ของ PUPE พร้อมแล้ว!');
 });
